4e178770a2eb31a45e297823eeca26b0369211a3
Shivam kumar tiwari <shivam55432@rediffmail.com>
2019-04-11 16:14:35 +0530

    Update MagicTest.java

--- MagicTest.java
+++ MagicTest.java
@@ -3,11 +3,22 @@ package com.myproject.sample;
 public class MagicTest
 {
 
+   /* 
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+    */
+    
     @Test
     public void testHelloWorld() {
         assertEquals("hello world", Magic.getHelloWorld());
     }
 
+    /*
+     public static int getNumber10(){
+        return 10;
+    }
+    */
    
      @Test
     public void testNumber10() {
