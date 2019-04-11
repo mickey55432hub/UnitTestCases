90cfd4e226c6d2aef8cbe1871fd8c5b1bcfe6a84
Shivam kumar tiwari <shivam55432@rediffmail.com>
2019-04-11 16:14:58 +0530

    Update Magic.java

--- Magic.java
+++ Magic.java
@@ -3,9 +3,29 @@ package com.myproject.sample;
 public class Magic
 {
 
-    public static int GetNumber()
-    {
-        return 7;
+   
+    public static String getHelloWorld(){
+        return "hello world";
     }
-
+    /*
+    @Test
+    public void testHelloWorld() {
+        assertEquals("hello world", Magic.getHelloWorld());
+    }
+    */
+    
+    
+    
+    public static int getNumber10(){
+        return 10;
+    }
+    /*
+     @Test
+    public void testNumber10() {
+        assertEquals(10, Magic.getNumber10());
+    }
+    */   
+    
+    
+    
 }
