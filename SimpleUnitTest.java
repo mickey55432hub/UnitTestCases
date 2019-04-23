4d64d15d7acf818491909640fe3da2cd517e5cba
Shivam kumar tiwari <shivam55432@rediffmail.com>
2019-04-23 16:50:51 +0530

    Create SimpleUnitTest.java

---
+++ SimpleUnitTest.java
@@ -0,0 +1,15 @@
+package com.adobe.aem.events;
+import static org.junit.Assert.*;
+import org.unit.Test;
+
+
+public class SimpleUnitTest
+{
+
+@Test
+public void someTest()
+{
+assertTrue(true);
+}
+}
+
